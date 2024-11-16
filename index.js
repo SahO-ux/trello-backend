81230ea67a9c6ca6432f55cf9978c0e50bbfd9c6
SahilAkbari <sahilakbari1111@gmail.com>
2024-11-16 20:50:05 +0530

    removed helmet

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import helmet from "helmet";
 import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
@@ -17,13 +14,6 @@ import {
   deleteTask,
 } from "./controllers/task.js";
 
-// Replace this with your deployed frontend URL
-const allowedOrigins = ["https://trello-frontend-delta.vercel.app"];
-
-// Configs for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // App setup
 dotenv.config();
 const app = express();
@@ -32,12 +22,9 @@ app.use(cors());
 
 // Middleware
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Vercel route
 app.get("/", (req, res) => {
